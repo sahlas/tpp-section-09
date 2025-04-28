60e633248d5c7c8a2b3969f4f0484162287ea600
bsahlas <bill.sahlas@gmail.com`>
2025-04-28 14:18:24 -0400

    adding a doc string

--- check-code-quality.sh
+++ check-code-quality.sh
@@ -7,7 +7,7 @@ black --config .black.toml . || ((EXIT_STATUS++))
 pylint --rcfile .pylintrc *.py || ((EXIT_STATUS++))
 flake8 --config .flake8 || ((EXIT_STATUS++))
 mypy . --exclude venv || ((EXIT_STATUS++))
-ruff check --config pyproject.toml --fix || ((EXIT_STATUS++))
+ruff check --config ruff.toml --fix || ((EXIT_STATUS++))
 # isort . --settings .isort.cfg --exclude venv || ((EXIT_STATUS++))
 
 echo ---- $EXIT_STATUS
