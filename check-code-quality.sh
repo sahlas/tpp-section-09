35e66cb933410602b2296c301895d7ccdcbdd678
bsahlas <bill.sahlas@gmail.com`>
2025-04-28 09:48:27 -0400

    catchup'

---
+++ check-code-quality.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+black --config .black.toml .
+pylint --rcfile .pylintrc *.py
+flake8 --config .flake8
+mypy . --exclude venv
+ruff check --config ruff.toml --fix
+isort . --settings .isort.cfg
---
+++ trial.py
@@ -0,0 +1,9 @@
+# def help():
+#     print("help")
+
+
+def help():
+    print("help")
+
+
+from __future__ import annotations
