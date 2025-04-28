af4c041fa0bb3815f6967c6f4b48c736cb9a3d11
bsahlas <bill.sahlas@gmail.com`>
2025-04-28 13:44:25 -0400

    delete pys

--- check-code-quality.sh
+++ check-code-quality.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 
+EXIT_STATUS=0
 
-black --config .black.toml .
-pylint --rcfile .pylintrc *.py
-flake8 --config .flake8
-mypy . --exclude venv
-ruff check --config ruff.toml --fix
-isort . --settings .isort.cfg --exclude venv
+black --config .black.toml . || ((EXIT_STATUS++))
+pylint --rcfile .pylintrc *.py || ((EXIT_STATUS++))
+flake8 --config .flake8 || ((EXIT_STATUS++))
+mypy . --exclude venv || ((EXIT_STATUS++))
+ruff check --config pyproject.toml --fix || ((EXIT_STATUS++))
+# isort . --settings .isort.cfg --exclude venv || ((EXIT_STATUS++))
+
+echo ---- $EXIT_STATUS
+exit $EXIT_STATUS
